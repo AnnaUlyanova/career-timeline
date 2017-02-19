b79373b2cb4a285fe65f6ea95d1dd56c5eed6bd0
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-02-19 22:44:14 +1300

    fix images and add git links

--- seeds/projects.js
+++ seeds/projects.js
@@ -11,28 +11,32 @@ exports.seed = function (knex, Promise) {
           tech: 'React, Redux, Webpack, Axios, Node.js, Express server, SQLite3, Knex.js, PostgreSQL.',
           description: 'Final group project, summary of the skills learned during the bootcamp.',
           project_url: 'http://www.goflat.co.nz/',
-          image_url: '/img/goflat.png'}),
+          image_url: '/img/goflat1.png',
+          git: 'https://github.com/AnnaUlyanova/flatme'}),
         knex('projects').insert({
           id: 2,
           name: 'New Zealand Great Adventure Hiking',
           tech: 'React, Webpack, Superagent, Node.js, Express server, Hanldebars.js, SQLite3, Knex.js, PostgreSQL, Google Maps API.',
           description: 'Personal project with client-side and server-side rendering. You can check map of NZ\'s 9 great walks, activities on the particular walk, suggestions what to pack, see current alerts, add notes.',
           project_url: 'https://nz-hiking-adventure.herokuapp.com/',
-          image_url: '/img/nz-hiking.png'}),
+          image_url: '/img/nz-hiking1.png',
+          git: 'https://github.com/AnnaUlyanova/nz-hiking'}),
         knex('projects').insert({
           id: 3,
           name: 'Espiral Dev Academy Blog | Phase 0',
           tech: 'Node.js, Express server, Hanldebars.js, SQLite3, Knex.js, PostgreSQL.',
           description: 'Server-side rendering application. Blog created during 9 weeks of the pre-bootcamp phase.',
           project_url: 'https://eda-blog.herokuapp.com/',
-          image_url: '/img/eda-blog.png'}),
+          image_url: '/img/eda-blog1.png',
+          git: 'https://github.com/AnnaUlyanova/eda-phase0-blog'}),
         knex('projects').insert({
           id: 4,
           name: 'Map of New Zealand\'s 9 Great Walks',
           tech: 'HTML5, CSS3, Javascript, jQuery.',
           description: 'Front-end application. First project created at the end of the pre-bootcamp phase.',
           project_url: 'https://annaulyanova.github.io/nz-great-walks-map',
-          image_url: '/img/nz-map.png'})
+          image_url: '/img/nz-map1.png',
+          git: 'https://github.com/AnnaUlyanova/nz-great-walks-map'})
       ])
     })
 }
