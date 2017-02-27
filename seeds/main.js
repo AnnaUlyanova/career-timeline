9edaf0dea227db62a39980b6868a8945ab8c487e
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-02-27 21:46:18 +1300

    change the order on the timeline

--- seeds/main.js
+++ seeds/main.js
@@ -6,40 +6,40 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('career-steps').insert({
-          id: 1,
+          id: 6,
           title: 'Moscow Power Engineering Institute (Technical University)',
           dates: 'September 2006 – June 2012',
           description: 'Qualification of Engineer with the Specialty in Information Systems and Rechnology',
           logo: '/img/mpei.png'}),
         knex('career-steps').insert({
-          id: 2,
+          id: 5,
           title: 'Croc Incorporated',
           dates: 'January 2011 – February 2013',
           description: 'Service Delivery Manager',
           type: 'work',
           logo: '/img/croc.png'}),
         knex('career-steps').insert({
-          id: 3,
+          id: 4,
           title: 'Auckland University of Technology',
           dates: 'February 2013 – December 2013',
           description: 'Master of Service-Oriented Computing',
           logo: '/img/aut.png'}),
         knex('career-steps').insert({
-          id: 4,
+          id: 3,
           title: 'Medtech Global',
           dates: 'March 2014 – December 2014',
           description: 'Operations Coordinator',
           type: 'work',
           logo: '/img/medtech.png'}),
         knex('career-steps').insert({
-          id: 5,
+          id: 2,
           title: 'Exeed Ltd',
           dates: 'April 2015 – October 2016',
           description: 'Sales Support and Operations',
           type: 'work',
           logo: '/img/exeed.png'}),
         knex('career-steps').insert({
-          id: 6,
+          id: 1,
           title: 'Enspiral Dev Academy',
           dates: 'October 2016 – February 2017',
           description: 'Web Development Programme',
